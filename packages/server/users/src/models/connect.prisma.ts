de68b6ffdb3aad0c35bb52e767cec6a0649e6979
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-29 06:17:49 -0300

    docs: :memo: Adicionando comentarios de funcionalidade

--- packages/server/users/src/models/connect.prisma.ts
+++ packages/server/users/src/models/connect.prisma.ts
@@ -1,5 +1,11 @@
 import {PrismaClient} from '@prisma/client';
 
+/**
+ * @constant prisma
+ * ? Estabelece uma conexão com o Prisma ORM instanciando a classe nativa do
+ * ? Prisma (PrismaClient)
+ */
+
 const prisma = new PrismaClient();
 
 export default prisma;
