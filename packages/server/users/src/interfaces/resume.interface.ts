3b59d839a70310775ee747b896aa03cd2c551ed6
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-29 16:18:47 -0300

    fix: :pencil2: Corrigindo a typagem da interface
    Corrigindo a tipagem da interface para seguir as alteraçoes feitas na tabela

--- packages/server/users/src/interfaces/resume.interface.ts
+++ packages/server/users/src/interfaces/resume.interface.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 type IResume = {
-	cpf: number;
-	birthDate: number;
+	cpf: string;
+	birthDate: string;
 	sex: string;
 	civilState: string;
 	schooling: string;
 	specialization?: string;
 	company?: string;
 	office?: string;
-	wage: number;
+	wage: string;
 };
 export default IResume;
