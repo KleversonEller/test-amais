c2aa4d975df18d72d30ae1b49bac1009036f573e
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-29 06:20:48 -0300

    feat: :sparkles: Criando middleware de validação
    Criando validaçoes utilizando o Joi

---
+++ packages/server/users/src/middleware/validate.resume.middleware.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+import Joi from 'joi';
+import {StatusCodes} from 'http-status-codes';
+import PersonalError from '@utils/Personal.error';
+import type IResume from 'src/interfaces/resume.interface';
+
+/**
+ * @function ResumeValidate
+ * ? Classe que faz as validações para a tabela Resume utilizando o Joi
+ *
+ * @method ResumeValidate.newResume
+ * ? Método que valida as informações para a criação/atualização de um currículo
+ */
+
+export default class ResumeValidate {
+	static newResume(data: IResume): void {
+		const {error} = Joi.object({
+			cpf: Joi.string().required().length(11),
+			birthDate: Joi.string().required().length(8),
+			sex: Joi.string().required(),
+			civilState: Joi.string().required(),
+			schooling: Joi.string().required(),
+			specialization: Joi.string(),
+			company: Joi.string(),
+			office: Joi.string(),
+			wage: Joi.string().required(),
+		}).validate(data);
+
+		if (error) {
+			console.log(error);
+			throw new PersonalError(StatusCodes.BAD_REQUEST, 'All fields must be filled');
+		}
+	}
+}
