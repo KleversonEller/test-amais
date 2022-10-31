ffd2f383821a2c0850b8c27841fd941fd0e5c0e7
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-31 09:05:21 -0300

    refactor: :recycle: Implementando as rotas da aplicação

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,12 +3,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Home from './pages/home';
 import NotFound from './pages/not-found';
+import Login from './pages/login';
+import NewUser from './pages/new-user';
+import UpdateResume from './pages/update-resume';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/create' element={<NewUser />} />
+				<Route path='/update' element={<UpdateResume />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
