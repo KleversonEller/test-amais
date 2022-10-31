818180715d9a6e44378906dc37b327ab2382feaf
Kleverson Eller <kleverson.a16@gmail.com>
2022-10-31 09:05:46 -0300

    refactor: :recycle: Adicionando novos stados globais

--- packages/web/src/store.tsx
+++ packages/web/src/store.tsx
@@ -1,17 +1,50 @@
 import create from 'zustand';
 import {devtools} from 'zustand/middleware';
 
+import type {IUserInfo, IResumeInfo} from './interface/user-info';
+
 type Store = {
 	logon: boolean;
-	setLogon: () => void;
+	token: string;
+	userInfo: IUserInfo | undefined;
+	resume: IResumeInfo;
+
+	setLogon: (state: boolean) => void;
+	setToken: (token: string) => void;
+	setUserInfo: (infos: IUserInfo | undefined) => void;
 };
 
 const useStore = create<Store>()(
 	devtools(set => ({
 		logon: false,
+		token: 'token',
+		userInfo: undefined,
+		resume: {
+			id: ' ',
+			cpf: ' ',
+			birthDate: ' ',
+			sex: ' ',
+			civilState: ' ',
+			schooling: ' ',
+			specialization: '',
+			company: '',
+			office: '',
+			wage: ' ',
+			userId: ' ',
+			createAt: ' ',
+		},
 
-		setLogon() {
-			set({logon: true});
+		setLogon(state) {
+			set({logon: state});
+		},
+
+		setToken(token) {
+			set({token});
+		},
+
+		setUserInfo(infos) {
+			set({userInfo: infos});
+			set({resume: infos?.Resume[0] as unknown as IResumeInfo});
 		},
 	})),
 );
