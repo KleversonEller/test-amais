05b4c4bcb63858a3278a99a0b23ec1a19092b042
Kleverson Eller <kleverson.a16@gmail.com>
2022-11-01 09:17:59 -0300

    refactor: :recycle: Fazendo verificação de usuario
    Implementando logica para validar se é um usuario ou um adiministrador que esta logando

--- packages/web/src/pages/home.tsx
+++ packages/web/src/pages/home.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import NavBar from '../components/nav-bar';
 import useStore from '../store';
 import UserResume from '../components/user-resume';
+import ClientSummary from '../components/client-summary';
 import CreateUser from '../components/create-user';
 
 function Home() {
 	const logon = useStore(state => state.logon);
+	const userInfo = useStore(state => state.userInfo);
 
 	return (
 		<div className='flex flex-col items-center h-screen'>
 			<NavBar buttonText={logon ? 'Sair' : 'Login'} />
 
-			{logon ? <UserResume /> : <CreateUser />}
+			{logon ? (userInfo?.permision === 'admin' ? <ClientSummary /> : <UserResume />)
+				: <CreateUser />}
 		</div>
 	);
 }
